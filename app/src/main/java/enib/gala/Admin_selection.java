847dee25e4c89ac2e0bb8da79c7d7d5f2350bc4d
phycelot <contact@antoninprouteau.fr>
2018-10-31 18:59:25 +0100

    add admin selection mode

--- app/src/main/java/enib/gala/Admin_selection.java
+++ app/src/main/java/enib/gala/Admin_selection.java
@@ -2,12 +2,45 @@ package enib.gala;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Admin_selection extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_selection);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            finish();
+        }
+
+    }
+
+    public void goToBarMode(View view)
+    {
+
+    }
+
+    public void goToEntranceMode(View view)
+    {
+
+    }
+
+    public void goToRechargeMode(View view)
+    {
+
     }
 }
